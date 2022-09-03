644338f4f26a414d6087697e1d451b5b6b4bac4b
Iago Cruz <iagosilvacontato@gmail.com>
2022-09-03 16:45:22 -0300

    variaveis

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print('*******************')
 print('Cadastro de Estoque')
 print('*******************')
 
+#variaveis
+produto = input('Digite o nome do produto: ')
+
+codigo = int(input('Insira o código do produto: '))
